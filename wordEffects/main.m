02cb7a3277d3620cb89c2a30b5f5bc880a27abd5
J.K <K_940201@hotmail.com>
2017-07-24 18:39:56 -0700

    canadianize

--- wordEffects/main.m
+++ wordEffects/main.m
@@ -10,8 +10,36 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        char inputChars[255];
+        
+        printf("input a String: ");
+        
+        fgets(inputChars, 255, stdin);
+        
+        printf("Your string is %s\n", inputChars);
+        
+        NSString *inputString = [NSString stringWithUTF8String:inputChars];
+        
+        NSLog(@"Input was: %@", inputString);
+        
+        NSString *upInputString = [inputString uppercaseString];
+        
+        NSLog(@"Make it louder: %@",upInputString);
+        
+        NSString *lowInputString = [inputString lowercaseString];
+        
+        NSLog(@"Make it more quiet: %@",lowInputString);
+        
+        NSNumber *convertNumber = inputString;
+        
+        NSLog(@"The number you put in is: %@",convertNumber);
+        
+        NSString *addEh = [inputString stringByAppendingString:@"eh?"];
+        
+        NSLog(@"Canadianize: %@",addEh);
+        
+        
     }
     return 0;
 }
